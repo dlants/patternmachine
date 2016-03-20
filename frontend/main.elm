868f0d9231224349388f7f40fc7766279a91b0aa
Denis Lantsman <denis@desmos.com>
2016-03-19 18:32:16 -0700

    elm style

--- frontend/main.elm
+++ frontend/main.elm
@@ -1,4 +1,4 @@
-module PatternMachine where
+module PatternMachine (..) where
 
 import Effects
 import Window
@@ -9,138 +9,195 @@ import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import StartApp
 
-app = StartApp.start
-  { init = (startModel, Effects.none)
-  , view = view
-  , update = update
-  , inputs = [resize]
-  }
 
-main = app.html
+app =
+  StartApp.start
+    { init = ( startModel, Effects.none )
+    , view = view
+    , update = update
+    , inputs = [ resize ]
+    }
+
+
+main =
+  app.html
+
+
 
 -- MODEL
-port windowSize: (Int, Int)
 
+
+port windowSize : ( Int, Int )
 type alias Model =
-  { matrix: Matrix.Matrix Int
-  , window: (Int, Int)
+  { matrix : Matrix.Matrix Int
+  , window : ( Int, Int )
   }
 
-startModel: Model
+
+startModel : Model
 startModel =
   { matrix = (Matrix.repeat 10 10 0)
   , window = windowSize
   }
 
+
 matrixRows matrix =
-  List.filterMap (\i -> Matrix.getRow i matrix) [0..Matrix.height(matrix) - 1]
+  List.filterMap (\i -> Matrix.getRow i matrix) [0..Matrix.height (matrix) - 1]
+
 
 addColumn matrix =
   Matrix.repeat 1 (Matrix.height matrix) 0
-  |> Matrix.concatHorizontal matrix
-  |> Maybe.withDefault matrix
+    |> Matrix.concatHorizontal matrix
+    |> Maybe.withDefault matrix
+
 
 addRow matrix =
   Matrix.repeat (Matrix.width matrix) 1 0
-  |> Matrix.concatVertical matrix
-  |> Maybe.withDefault matrix
+    |> Matrix.concatVertical matrix
+    |> Maybe.withDefault matrix
+
+
 
 -- +1 for the add row/col
-boxsize: Model -> Int
+
+
+boxsize : Model -> Int
 boxsize model =
   let
-    (width, height) = model.window
-    (cols, rows) = model.matrix.size
+    ( width, height ) =
+      model.window
+
+    ( cols, rows ) =
+      model.matrix.size
   in
-    Basics.min (width//(cols+1)) (height//(rows+1))
+    Basics.min (width // (cols + 1)) (height // (rows + 1))
 
-colors: Array.Array String
-colors = Array.fromList
-  [ "#22a68a"
-  , "#7eb7d5"
-  , "#b696c8"
-  ]
 
-  {--
-  , "#bdd730"
-  , "#9bccb4"
-  , "#fedf4a"
-  , "#556896"
-  , "#f28f4a"
-  , "#e93830"
-  , "#f6dec6"
-  ]--}
+colors : Array.Array String
+colors =
+  Array.fromList
+    [ "#22a68a"
+    , "#7eb7d5"
+    , "#b696c8"
+    ]
+
+
+
+{- -
+, "#bdd730"
+, "#9bccb4"
+, "#fedf4a"
+, "#556896"
+, "#f28f4a"
+, "#e93830"
+, "#f6dec6"
+]-
+-}
+
 
 getColor val =
   Array.get (val % Array.length colors) colors
-  |> Maybe.withDefault "red"
+    |> Maybe.withDefault "red"
+
+
 
 -- UPDATE
-type Action = NoOp
-            | Increment Int Int
-            | AddRow
-            | AddColumn
-            | Resize (Int, Int)
+
+
+type Action
+  = NoOp
+  | Increment Int Int
+  | AddRow
+  | AddColumn
+  | Resize ( Int, Int )
+
 
 update action model =
   let
     newModel =
       case action of
-        NoOp -> model
+        NoOp ->
+          model
+
         Increment row col ->
-          {model | matrix = Matrix.update col row (\val -> val + 1) model.matrix}
+          { model | matrix = Matrix.update col row (\val -> val + 1) model.matrix }
+
         AddRow ->
-          {model | matrix = addRow model.matrix}
+          { model | matrix = addRow model.matrix }
+
         AddColumn ->
-          {model | matrix = addColumn model.matrix}
+          { model | matrix = addColumn model.matrix }
+
         Resize window ->
-          {model | window = window}
+          { model | window = window }
   in
-    (newModel, Effects.none)
+    ( newModel, Effects.none )
+
+
 
 -- INPUTS
-resize: Signal Action
+
+
+resize : Signal Action
 resize =
   Signal.map Resize Window.dimensions
 
+
+
 -- VIEW
+
+
 boxStyle val size =
   let
-      sizeStr = (toString size) ++ "px"
+    sizeStr =
+      (toString size) ++ "px"
   in
-    style [
-      ("width", sizeStr),
-      ("height", sizeStr),
-      ("margin", "1px"),
-      ("background-color", getColor val)
-    ]
+    style
+      [ ( "width", sizeStr )
+      , ( "height", sizeStr )
+      , ( "margin", "1px" )
+      , ( "background-color", getColor val )
+      ]
+
 
 drawBox address size rowidx colidx val =
-  div [Html.Events.onClick address (Increment rowidx colidx)
+  div
+    [ Html.Events.onClick address (Increment rowidx colidx)
     , boxStyle val size
-    ] []
+    ]
+    []
+
 
 drawRow address size rowidx row =
-  div [style [("display", "flex")]] (Array.toList (Array.indexedMap (drawBox address size rowidx) row))
+  div [ style [ ( "display", "flex" ) ] ] (Array.toList (Array.indexedMap (drawBox address size rowidx) row))
+
 
 drawMatrix address size matrix =
   div [] (List.indexedMap (drawRow address size) (matrixRows matrix))
 
+
 drawAddBox address clickAction =
-  div [ Html.Events.onClick address clickAction
-      , style [("background-color", "rgba(0,0,0,0.25)")
-              , ("color", "white")
-              ]
-      ]
-      [text "+"]
+  div
+    [ Html.Events.onClick address clickAction
+    , style
+        [ ( "background-color", "rgba(0,0,0,0.25)" )
+        , ( "color", "white" )
+        ]
+    ]
+    [ text "+" ]
+
 
 view address model =
   let
-      size = boxsize model
+    size =
+      boxsize model
   in
-    div [] [
-      div [style [("display", "flex")]] [(drawMatrix address size model.matrix)
-             , (drawAddBox address AddColumn)
-             ]
+    div
+      []
+      [ div
+          [ style [ ( "display", "flex" ) ] ]
+          [ (drawMatrix address size model.matrix)
+          , (drawAddBox address AddColumn)
+          ]
       , (drawAddBox address AddRow)
       ]
